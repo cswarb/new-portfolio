6f2b9a882765b89f2b19d5a9e351c56f907707e8
Chris Swarbrick <chris.swarb@gmail.com>
2017-08-29 21:35:49 +0100

    [development] - Tidied logic - Added trigger points to transition to new images

--- src/app/post/fixed-post-block.directive.ts
+++ src/app/post/fixed-post-block.directive.ts
@@ -12,36 +12,19 @@ export class FixedPostBlockDirective implements OnInit {
 	private anchorPointEnd;
 	private anchorStartRef;
 
-	@HostBinding('class.resting') resting: any = false;
-	@HostBinding('class.fixed') fixed: any = false;
-	@HostBinding('class.end') end: any = false;
+	@HostBinding("class.resting") resting: any = false;
+	@HostBinding("class.fixed") fixed: any = false;
+	@HostBinding("class.end") end: any = false;
 
 	@HostListener("window:scroll", this.onWindowScroll)
 	public onWindowScroll() {
-		if(document.body.scrollTop >= (this.anchorPointStart.top - this.vhToPixel(5))) {
-			this.resting = false;
-			this.fixed = true;
-			this.end = false;
-			this.element.nativeElement.style.left = (this.anchorPointStart.left + "px");
-			this.element.nativeElement.style.width = (this.anchorPointStartElement.offsetWidth.toString() + "px");
-		} else {
-			this.resting = true;
-			this.fixed = false;
-			this.end = false;
-		}
+		this.initialiseScrollingLogic();
+	}
 
-		// console.log(window.pageYOffset >= this.anchorPointStart.top);
-		//Makes sure it sticks at the last paragraph element
-		if(window.pageYOffset >= (this.anchorPointEnd.top - this.vhToPixel(5))) {
-			this.resting = false;
-			this.fixed = false;
-			this.end = true;
-			this.element.nativeElement.style.top = ((this.anchorPointEnd.top - this.anchorPointStart.top) + "px");
-			this.element.nativeElement.style.width = (this.anchorPointStartElement.offsetWidth.toString() + "px");
-		}
-
-		
-
+	@HostListener("window:resize", this.onWindowResize)
+	public onWindowResize() {
+		this.anchorPointStart = this.getOffset(this.anchorPointStartElement);
+		this.anchorPointEnd = this.getOffset(this.anchorPointEndElement);
 	}
 
 	constructor(private element: ElementRef) { 
@@ -52,22 +35,56 @@ export class FixedPostBlockDirective implements OnInit {
 	ngOnInit() {
 		this.anchorPointStartElement = document.getElementById("scroll-anchor-ref");
 		this.anchorPointEndElement = document.getElementById("scroll-anchor-ref-end");
-
 		this.anchorPointStart = this.getOffset(this.anchorPointStartElement);
 		this.anchorPointEnd = this.getOffset(this.anchorPointEndElement);
 	}
 
-	public getOffset(el: any) {
+	public initialiseScrollingLogic(): void {
+		if(document.body.scrollTop >= (this.anchorPointStart.top - this.vhToPixel(5))) {
+			this.setState("fixed");
+			this.element.nativeElement.style.left = this.pxString(this.anchorPointStart.left);
+			this.element.nativeElement.style.width = this.pxString(this.anchorPointStartElement.offsetWidth);
+		} else {
+			this.setState("resting");
+		};
+
+		//Makes sure it stops at the last paragraph element
+		if(window.pageYOffset >= (this.anchorPointEnd.top - this.vhToPixel(5))) {
+			this.setState("end");
+			this.element.nativeElement.style.top = (this.pxString(this.anchorPointEnd.top - this.anchorPointStart.top));
+			this.element.nativeElement.style.width = this.pxString(this.anchorPointStartElement.offsetWidth);
+		};
+	}
+
+	public getOffset(el: any): any {
 		if(!el) return;
 		el = el.getBoundingClientRect();
 		return {
-			left: el.left + window.scrollX,
-			top: el.top + window.scrollY
+			"left": el.left + window.scrollX,
+			"top": el.top + window.scrollY
 		}
 	}
 
 	public vhToPixel(multiplier: number): number {
 		return (window.innerHeight / 100) * multiplier;
+	}
+
+	public pxString(value: number): string {
+		return value.toString() + "px";
+	}
+
+	public setState(state: string): void {
+		switch(state) {
+			case "fixed":
+				this.resting = this.end = false; this.fixed = true;
+			break;
+			case "resting":
+				this.resting = true; this.fixed = this.end = false;
+			break;
+			case "end":
+				this.resting = this.fixed = false; this.end = true;
+			break;
+		};
 	}
 
 }
