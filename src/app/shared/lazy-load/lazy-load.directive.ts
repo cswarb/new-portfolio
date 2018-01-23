a143c4b09a05b4a07ac49c696baf04ce0cd8adf7
Chris Swarbrick <chris.swarb@gmail.com>
2018-01-23 22:15:50 +0000

    [development-alt] - Added res attr to append to the image so the browser can serve relevant image when it detects high dpi devices

--- src/app/shared/lazy-load/lazy-load.directive.ts
+++ src/app/shared/lazy-load/lazy-load.directive.ts
@@ -9,6 +9,7 @@ export class LazyLoadDirective implements OnInit, AfterViewInit {
 	private image: any = null;
 	private imageSrc: string = null;
 	private isSrcSet: boolean = false;
+	private res: number = 1;
 
 	constructor(
 	private element: ElementRef
@@ -24,14 +25,17 @@ export class LazyLoadDirective implements OnInit, AfterViewInit {
 		this.image = this.element.nativeElement;
 		this.imageSrc = this.element.nativeElement.dataset["lazySrc"];
 		this.isSrcSet = this.element.nativeElement.dataset["isSrcSet"];
+		this.res = this.element.nativeElement.dataset["res"];
 
 		window.addEventListener("scroll", this.inViewCallback.bind(this));
 	}
 
-	public inViewCallback() {
+	public inViewCallback(): void {
 		if (this.isInViewport(this.image)) {
 			//Set the image src
-			this.isSrcSet ? this.element.nativeElement.setAttribute("srcset", this.imageSrc) : this.element.nativeElement.setAttribute("src", this.imageSrc);
+			this.isSrcSet ? 
+				this.element.nativeElement.setAttribute("srcset", this.imageSrc + this.getRes()) : 
+				this.element.nativeElement.setAttribute("src", this.imageSrc);
 
 			//Fade in the span above to make it less jarring
 			this.element.nativeElement.parentElement.classList.add("lazyimage--loaded");
@@ -41,7 +45,7 @@ export class LazyLoadDirective implements OnInit, AfterViewInit {
 		};
 	}
 
-	private isInViewport(el: any) {
+	private isInViewport(el: any): boolean {
 		var image = el.getBoundingClientRect();
 		return (
 			image.bottom >= 0 &&
@@ -55,6 +59,10 @@ export class LazyLoadDirective implements OnInit, AfterViewInit {
 			window.innerWidth ||
 			document.documentElement.clientWidth)
 		);
+	}
+
+	private getRes(): string {
+		return " " + this.res + "x";
 	}
 
 }
