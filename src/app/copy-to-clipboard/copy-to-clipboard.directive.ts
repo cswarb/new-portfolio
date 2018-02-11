489abb31e725a85f7f4c66cc0532119c05861eb5
Chris Swarbrick <chris.swarb@gmail.com>
2018-02-11 01:02:19 +0000

    [development-alt] - Update css order - Update email copy directive - Revert index layout

--- src/app/copy-to-clipboard/copy-to-clipboard.directive.ts
+++ src/app/copy-to-clipboard/copy-to-clipboard.directive.ts
@@ -4,38 +4,37 @@ import { Directive, ElementRef, HostListener } from "@angular/core";
   selector: "[data-copy-to-clipboard]"
 })
 export class CopyToClipboardDirective {
-
 	private copied = false;
+	private timeout: number = 5000;
 
 	@HostListener("click", ["$event"]) onClick($event) {
         this.copy($event);
     }
 
-	constructor(private element: ElementRef) {
+	constructor(
+		private element: ElementRef
+	) {
 		
 	}
 
 	private copy($e) {
-		console.log($e);
         try {
             $e.target.select();           
             const successful = document.execCommand("copy");
             this.copied = true;
-            this.toggleClass(true, $e.target);
+			this.toggleClass($e.target, "copied");
         } catch (error) {
         	this.copied = false;
-        	this.toggleClass(false, $e.target);
-            console.log("Failed to copy text. ", error);
+			this.toggleClass($e.target, "not-copied");
+            console.error("Failed to copy text: ", error);
         };
 	}
 
-	private toggleClass(isCopied: boolean, $elem: any) {
-		if(isCopied) {
-			$elem.classList.add("copied");
-		};
+	private toggleClass($elem: Element, appendedClass: string) {
+		$elem.classList.add(appendedClass);
 		setTimeout(() => {
-			$elem.classList.remove("copied");
-		}, 5000);
+			$elem.classList.remove(appendedClass);
+		}, this.timeout);
 	}
 
 }
