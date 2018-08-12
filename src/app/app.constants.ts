49ec04936af914cd0f45c65ba51532f24cfd87e9
Chris Swarbrick <chris.swarb@gmail.com>
2018-08-12 14:47:21 +0100

    [development-alt] - Added portfolio images

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,7 +1,7 @@
 export class AppConstants {
 	BASE_URL = "./";
-	CONTENT_DIR = "/content/";
-	POSTS_CONTENT_DIR = "/content/posts/";
-	PHOTO_STORY_CONTENT_DIR = "/content/photo-story/";
-	BLOG_CONTENT_DIR = "/content/blog/";
+	CONTENT_DIR = "content/";
+	POSTS_CONTENT_DIR = "content/posts/";
+	PHOTO_STORY_CONTENT_DIR = "content/photo-story/";
+	BLOG_CONTENT_DIR = "content/blog/";
 }
